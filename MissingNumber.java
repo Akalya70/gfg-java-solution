8ff5c41697cf3c47a7d24825e8737783d43f43fb
AKALYA <akalyamurugesan007@gmail.com>
2026-09-09 22:16:17 +0530

    Fix method declaration formatting in MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,5 +1,6 @@
 class Solution {
-    int missingNum(int arr[]) {
+    int missingNum(int arr[]) 
+    {
         int n=arr.length+1;
         long expected=(long) n*(n+1)/2;
         long actual=0;
